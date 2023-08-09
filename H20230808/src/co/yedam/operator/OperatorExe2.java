1caf0a0d9f0462360f7074a2c186b353445835b6
admin <admin@YD5-18>
2023-08-09 09:34:33 +0900

    0808 숙제 최종

--- H20230808/src/co/yedam/operator/OperatorExe2.java
+++ H20230808/src/co/yedam/operator/OperatorExe2.java
@@ -24,22 +24,36 @@ public class OperatorExe2 {
 				if (insert > 100000) {
 					System.out.println("10만원 이상은 불가합니다");
 					continue;
-				} else if (insert < 0) {
-					System.out.println("마이너스 금액은 불가합니다");
-				// 코드작성
-			} else if (menu == 2) {
-				System.out.println("출금액 입력>> ");
-				if (insert > 100000) {
-					System.out.println("10만원 이상은 불가합니다");
+				} else if (balance + insert > 100000) {
+					System.out.println("총 잔액이 10만원을 초과합니다");
 					continue;
 				} else if (insert < 0) {
 					System.out.println("마이너스 금액은 불가합니다");
+					continue;
+				} else {
+					balance += insert;
+					System.out.printf("입금액은 %d원이고 잔액은 %d원입니다.\n", insert, balance);
+					continue;
+				}
 				// 코드작성
+			} else if (menu == 2) {
+				System.out.print("출금액 입력>> ");
+				int insert = Integer.parseInt(scn.nextLine());
+				if (insert > balance) {
+					System.out.printf("잔액 %d원을 초과할 수 없습니다\n", balance);
+					continue;
+				} else if (insert < 0) {
+					System.out.println("마이너스 금액은 불가합니다");
+				} else {
+					balance -= insert;
+					System.out.printf("%d원 출금했고 잔액은 %d원 남았습니다.\n", insert, balance);
+				// 코드작성
+				}
 			} else if (menu == 3) {
-				
+				System.out.printf("잔액은 %d원 입니다.\n", balance);
 				// 코드작성
-			}
 			} else if (menu == 4) {
+				System.out.println("작업을 종료하겠습니다.");
 				break;
 				// 코드작성
 			}	
